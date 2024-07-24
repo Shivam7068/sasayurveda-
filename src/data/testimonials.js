060b6e253794a461f28ef64d09b613fbabe65960
pankaj upadhyay <pankajup.git01@gmail.com>
2024-07-24 12:46:10 +0530

    data updated

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,22 +2,22 @@ export const testimonials = [
     // Add your testimonial data here
     {
         name: 'AKASH SINGH ',
-        quote: "At Bapu Ayurvedic College & Hospital, we are more than just an institution; we are a nurturing ground for dreams, a beacon of learning, and a catalyst for transformation. But don't just take our word for it. Hear directly from the voices that  matter  ",
+        quote: "At SAS Ayurvedic College & Hospital, we are more than just an institution; we are a nurturing ground for dreams, a beacon of learning, and a catalyst for transformation. But don't just take our word for it. Hear directly from the voices that matter",
         image: '/assets/avatar.svg',
     },
     {
         name: 'AMIT ANAND',
-        quote: "At Bapu Ayurvedic College & Hospital, the journey from student to healer is not just about acquiring knowledge; it's about embracing a way of life guided by compassion, integrity, and a relentless pursuit of excellence.",
+        quote: "At SAS Ayurvedic College & Hospital, the journey from student to healer is not just about acquiring knowledge; it's about embracing a way of life guided by compassion, integrity, and a relentless pursuit of excellence.",
         image: '/assets/avatar.svg',
     },
     {
         name: 'ANKITA GIRI',
-        quote: "Bapu Ayurvedic College has been a sanctuary for my academic and personal growth. The supportive environment and dedicated faculty have not only nurtured my passion for Ayurveda but have also instilled in me the confidence.",
+        quote: "Choosing SAS Ayurvedic College & Hospital was one of the best decisions of my life. The vibrant campus life, coupled with a diverse community of peers, has not only broadened my horizons but has also fostered lifelong connections.",
         image: '/assets/avatar.svg',
     },
     {
         name: 'DIKSHA MISHRA',
-        quote: "Choosing Bapu Ayurvedic College & Hospital was one of the best decisions of my life. The vibrant campus life, coupled with a diverse community of peers, has not only broadened my horizons but has also fostered lifelong connections.",
+        quote: "Choosing SAS Ayurvedic College & Hospital was one of the best decisions of my life. The vibrant campus life, coupled with a diverse community of peers, has not only broadened my horizons but has also fostered lifelong connections.",
         image: '/assets/avatar.svg',
     },
     // ... more testimonials
--- src/ui_component/common_comp/VerticalMarquee.jsx
+++ src/ui_component/common_comp/VerticalMarquee.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const VerticalMarquee = ({ items, type }) => {
-    console.log(items.length);
     return (
         <div className="relative overflow-hidden h-64 w-full bg-slate-400">
             <div className='flex justify-center bg-green-900 absolute w-full p-3 items-center text-white text-2xl font-medium z-[10]'>{type}</div>
