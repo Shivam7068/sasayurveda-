58ccf182a70651487ce17b000d8f1d6f4030d8f8
pankaj upadhyay <pankajup.git01@gmail.com>
2024-07-24 12:43:12 +0530

    news added

--- src/ui_component/common_comp/VerticalMarquee.jsx
+++ src/ui_component/common_comp/VerticalMarquee.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const VerticalMarquee = ({ items, type }) => {
+    console.log(items.length);
     return (
         <div className="relative overflow-hidden h-64 w-full bg-slate-400">
             <div className='flex justify-center bg-green-900 absolute w-full p-3 items-center text-white text-2xl font-medium z-[10]'>{type}</div>
-            <div className="animate-marquee  ">
-                {items.map((item, index) => (
-                    <div key={index} className="py-2 z-[-1] text-center hover:animation-pause">
-                        {item}
+            {
+                items.length > 0 ?
+                    <div className="animate-marquee hover:[animation-play-state:paused] ">
+                        {items.map((item, index) => (
+                            <div key={index} className="py-2 z-[-1] text-center ">
+                                {item}
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                    :
+                    <div className='flex  justify-center m-auto items-center h-full text-green-900 font-bold text-2xl '>
+                        No vacancies
+
+                    </div>
+            }
+
         </div>
     );
 };
--- src/ui_component/home_com/NewsBoard.jsx
+++ src/ui_component/home_com/NewsBoard.jsx
@@ -5,15 +5,15 @@ import Heading from '../common_comp/Heading';
 const NewsBoard = () => {
     const items1 = ['Holiday on 29-06-2023 (Bakrid)',
         'BAMS(2021 - 22) Examination From Fill Here', ' BAMS UP UG Counseling 2023-24 click here to registration', 'BAMS UP UG AYUSH Counseling Details (2023-24)', 'BAMS FIRST YEAR(2021 - 22) EXAMINATION form Open 20-06 - 2023'];
-    const items2 = ['Item 1-1', 'Item 1-2', 'Item 1-3', 'Item 1-4', 'Item 1-5'];
-    const items3 = ['Item 1-1', 'Item 1-2', 'Item 1-3', 'Item 1-4', 'Item 1-5'];
+    const items2 = ['BAMS UP UG Counseling 2023-24 click here to registration', 'BAMS UP UG AYUSH Counseling Details (2023-24)', 'BAMS FIRST YEAR (2021-22) EXAMINATION form Open 20-06-2023', 'Holiday on 29-06-2023 (Bakrid)', 'BAMS (2021-22) Examination From Fill Here .'];
+    const items3 = [];
     return (
         <div className='flex gap-5 flex-col'>
             <Heading name={"Notice"} />
             <div className="flex gap-3">
                 <VerticalMarquee items={items1} type={"News"} />
                 <VerticalMarquee items={items2} type={"Notice"} />
-                <VerticalMarquee items={items3} type={"Download"} />
+                <VerticalMarquee items={items3} type={"Vacancies"} />
             </div>
         </div>
     )
