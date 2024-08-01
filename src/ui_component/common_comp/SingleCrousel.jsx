ec1d05acae6ff646fdb9dee19e65cb93bdcebb23
pankaj upadhyay <pankajup.git01@gmail.com>
2024-08-01 17:30:19 +0530

    wave added

--- src/ui_component/common_comp/SingleCrousel.jsx
+++ src/ui_component/common_comp/SingleCrousel.jsx
@@ -11,34 +11,30 @@ import inner from "/assets/lab.avif";
 
 
 const SingleCrousel = () => {
-    const heroimages = [front, front, front, front];
+    const heroimages = [front, front, front];
 
     // , students, body, top, lab, inner
     return (
-        <div className="hero hero-img relative ">
-            <Carousel showArrows infiniteLoop showThumbs={false} autoPlay={true} >
+        <div className="relative">
+            <Carousel showArrows infiniteLoop showThumbs={false} showIndicators={false}>
                 {heroimages.map((image, index) => (
-
-                    <div key={index} className=''>
+                    <div key={index} className='relative'>
                         <img
-                            className=" h-[100vh] w-[100vw]"
+                            className="h-[85vh] w-full object-cover"
                             src={image}
+                            alt={`Slide ${index + 1}`}
                         />
+                        <div className="absolute left-0 inset-0 flex items-start justify-start">
+                            <div className="bg-black bg-opacity-50 p-4 rounded text-white w-full h-full ">
+                                <span className=' md:text-[1.5rem] font-bold'>Embark on a Journey of Healing with</span>
+                                <h1 className='font-bold md:text-[6rem]'>Ayurveda</h1>
+                            </div>
+                        </div>
 
                         <div className='bg-black'>
 
-                            <div className="right_clip absolute top-0 w-[100vw] md:w-[40vw] bg-[linear-gradient(279deg,_#000000a8,black)] h-full text-white flex flex-col  justify-center">
-                                <span className=' md:text-[1.5rem] font-bold'>Embark on a Journey of Healing with</span>
-                                <h1 className='font-bold md:text-[6rem]'>Ayurveda</h1>
-                                <p
-                                    style={{
-                                        fontSize: "1.2rem",
-                                        fontWeight: "500",
-                                        marginTop: "10px",
-                                    }}
-                                >
-                                    ADMISSIONS ARE NOW OPEN FOR 2024-2025
-                                </p>
+                            <div className=" fixed top-[50vh] w-[100vw] md:w-[100vw] bg-white h-[35vh] text-white flex flex-col right_clip ">
+
                             </div>
                         </div>
                     </div>
@@ -49,3 +45,18 @@ const SingleCrousel = () => {
 }
 
 export default SingleCrousel
+
+
+
+
+{/* <span className=' md:text-[1.5rem] font-bold'>Embark on a Journey of Healing with</span>
+<h1 className='font-bold md:text-[6rem]'>Ayurveda</h1>
+<p
+    style={{
+        fontSize: "1.2rem",
+        fontWeight: "500",
+        marginTop: "10px",
+    }}
+>
+    ADMISSIONS ARE NOW OPEN FOR 2024-2025
+</p> */}
